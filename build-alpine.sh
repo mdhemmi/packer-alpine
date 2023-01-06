8086cce5d75d9f72f6e3dd081b7976d649bf6e2f
hemmi <hemmi275@xmsoft.de>
2023-01-06 17:16:43 +0100

    change to vsphere-iso provider; alpine version 3.17

--- build-alpine.sh
+++ build-alpine.sh
@@ -2,7 +2,16 @@
 
 rm -rf output-alpine-* 
 
+#export PATH=$HOME/bin:/usr/local/bin:/Applications/VMware\ OVF\ Tool:$PATH
+#alias ovftool="/Applications/VMware\ OVF\ Tool/ovftool"
+#alias packer="~/Documents/photonos-appliance/packer"
+export PACKER_LOG=1
+export PACKER_LOG_PATH="./logs/build.log"
+
+### Check for dir, if not found create it using the mkdir ##
+[ ! -d "./logs" ] && mkdir -p "./logs"
+
 packer build \
     --var-file="alpine-builder.json" \
-    --var-file="alpine-3.15.6.json" \
+    --var-file="alpine-3.17.0.json" \
     alpine.json
